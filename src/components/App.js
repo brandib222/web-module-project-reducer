894091e79314415317a35da889d4503a9113df8e
brandib222 <brandi.danielle.ball@gmail.com>
2022-01-17 15:00:35 -0500

    addition is working

--- src/components/App.js
+++ src/components/App.js
@@ -1,5 +1,6 @@
 import React, { useReducer } from 'react';
 import  reducer, { initialState } from '../reducers';
+import { applyNumber } from '../actions';
 
 import './App.css';
 
@@ -9,6 +10,10 @@ import CalcButton from './CalcButton';
 function App() {
 
   const [ state, dispatch ] = useReducer(reducer, initialState);
+
+  const handleNumberClick = (number) => {
+    dispatch(applyNumber(number));
+  }
 
   return (
     <div className="App">
@@ -33,21 +38,21 @@ function App() {
             </div>
 
             <div className="row">
-              <CalcButton value={1}/>
-              <CalcButton value={2}/>
-              <CalcButton value={3}/>
+              <CalcButton value={1} onClick={() => handleNumberClick(1)}/> 
+              <CalcButton value={2} onClick={() => handleNumberClick(2)}/>
+              <CalcButton value={3} onClick={() => handleNumberClick(3)}/>
             </div>
 
             <div className="row">
-              <CalcButton value={4}/>
-              <CalcButton value={5}/>
-              <CalcButton value={6}/>
+              <CalcButton value={4} onClick={() => handleNumberClick(4)}/>
+              <CalcButton value={5} onClick={() => handleNumberClick(5)}/>
+              <CalcButton value={6} onClick={() => handleNumberClick(6)}/>
             </div>
 
             <div className="row">
-              <CalcButton value={7}/>
-              <CalcButton value={8}/>
-              <CalcButton value={9}/>
+              <CalcButton value={7} onClick={() => handleNumberClick(7)}/>
+              <CalcButton value={8} onClick={() => handleNumberClick(8)}/>
+              <CalcButton value={9} onClick={() => handleNumberClick(9)}/>
             </div>
 
             <div className="row">
